156d21cce1ac4fcc43ab8c01ac0fa0ffe67a63b6
Misael Aon <misaelaon10@gmail.com>
2022-10-14 20:46:08 -0300

    piloto de mobile con JS

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,22 @@ const menuNavegacion = document.querySelector('.contenedor__menu--lista');
 const btnCerrar = document.querySelector('.boton-cerrar');
 const btnWsp = document.querySelector('.btn-wsp');
 const clientes = document.querySelector('#clientes');
+const formulario = document.querySelector('#formulario');
+const cajaComentario = document.querySelector('#comentarios');
+const btnEnviar = document.querySelector('#btn-submit');
+const cabecera = document.querySelector('.cabecera');
+let comentarios = [];
 
 registroDeEventos();
 function registroDeEventos() {
     btn.addEventListener('click', mostrarMenu);
     btnCerrar.addEventListener('click', cerrarMenu);
     window.addEventListener('scroll', quitarWsp)
+    formulario.addEventListener('submit', agregarComentario)
+    document.addEventListener('DOMContentLoaded', () => {
+        comentarios = JSON.parse(localStorage.getItem('comentario')) || [];
+        mostrarComentario();
+    })
 }
 
 function mostrarMenu(e) {
@@ -32,3 +42,55 @@ function quitarWsp() {
     }
 }
 
+function agregarComentario(e) {
+    e.preventDefault();
+    const valorComentario = document.querySelector('#comentario').value;
+    const nombre = document.querySelector('#nombre').value;
+    if(valorComentario === '' || nombre === '') {
+        mensajeError('Todos los campos son obligatorios');
+        return;
+    }
+    const comentarioObj = {
+        id: Date.now(),
+        nombre,
+        comentario: valorComentario
+    };
+    comentarios = [...comentarios, comentarioObj];
+    mostrarComentario();
+}
+
+// Mensaje de error
+function mensajeError(error) {
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+    formulario.appendChild(mensajeError)
+
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000)
+}
+
+//Mostrar comentario en el HTML
+function mostrarComentario() {
+    limpiarHTML();
+
+    if(comentarios.length > 0) {
+        comentarios.forEach(comentario => {
+            const comentarios2 = document.createElement('p');
+            comentarios2.innerHTML = `${comentario.nombre}: <br/> ${comentario.comentario}`
+            cajaComentario.appendChild(comentarios2);
+        })
+    }
+    sincronizarLocalStorage();
+}
+
+function sincronizarLocalStorage() {
+    localStorage.setItem('comentario', JSON.stringify(comentarios));
+}
+
+function limpiarHTML() {
+    while(cajaComentario.firstChild) {
+        cajaComentario.removeChild(cajaComentario.firstChild);
+    }
+}
